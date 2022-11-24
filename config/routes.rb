ee5963aaef62722dc5fa82b0ada360e579ccb848
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-11-24 16:31:02 +0200

    initial with user interface

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  Rails.application.routes.draw do
+    devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      passwords: 'users/passwords',
+      registrations: 'users/registrations',
+      confirmations: 'users/confirmations'
+    }
+  end
+  root 'home#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
